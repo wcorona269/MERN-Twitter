540fb22fc63421f759cd21a52da35b67e0ca3002
wcorona269 <wcorona269@gmail.com>
2022-05-08 10:48:59 -0400

    routes

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const mongoose = require('mongoose');
-
 // creates a new Express server
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const db = require("./config/keys").mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 // route setup
-app.get("/", (req, res) => res.send(" go"));
+app.get("/", (req, res) => {
+	debugger;
+	res.send(" go")
+})
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 // tell our app which port to run on
 const port = process.env.PORT || 3000;
